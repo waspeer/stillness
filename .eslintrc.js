c67213c6e847b1bf8f293cd0767cfec392861895
Wannes Salomé <mail@wannessalome.nl>
2020-02-07 01:20:20 +0100

    Optimize rules to work well with prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,12 +2,11 @@ module.exports = {
   extends: ['./node_modules/poetic/config/eslint/eslint-config.js', 'plugin:cypress/recommended'],
   rules: {
     '@typescript-eslint/explicit-function-return-type': 'off',
-    'comma-dangle': ['error', 'always-multiline'],
     'import/no-extraneous-dependencies': [
       'error',
       { devDependencies: ['**/*.test.tsx', 'test/**/*'] },
     ],
-    quotes: ['error', 'single', { allowTemplateLiterals: false }],
+    quotes: ['error', 'single', { avoidEscape: true, allowTemplateLiterals: false }],
   },
   settings: {
     'import/resolver': {
